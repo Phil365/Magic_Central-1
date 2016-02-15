82363a0b793abdcac7a226d577f2f6de7a57d09a
Josianne Thessereault <josianne.thessereault@hotmail.com>
2016-02-15 14:33:10 -0500

    Scène victoire

---
+++ Assets/Scripts/scGrimperPont.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+/*fonction pour surélever le perso lorquil traverse un pont*/
+
+function OnTriggerEnter(heros:Collider) {
+	if (heros.tag == "Hero") {
+		heros.transform.rotation.x+= 3;
+	}
+}
+
+function OnTriggerExit(heros:Collider) {
+	if (heros.tag == "Hero") {
+		heros.transform.rotation.x-= 3;
+	}
+}
